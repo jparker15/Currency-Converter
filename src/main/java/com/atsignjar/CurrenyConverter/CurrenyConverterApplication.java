7e4165c90cdf303fcac5c8c5cfc448b7a1af3bdd
Jeremy <jeremyeparker@yahoo.com>
2022-05-04 14:07:40 -0400

    spring.io

---
+++ src/main/java/com/atsignjar/CurrenyConverter/CurrenyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.atsignjar.CurrenyConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenyConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsignjar/CurrenyConverter/CurrenyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignjar.CurrenyConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
